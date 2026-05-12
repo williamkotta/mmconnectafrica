f595af600c35e440d0caefbab9ef05e21e92f2db
williamkotta <architectwills@gmail.com>
2026-05-13 00:18:02 +0300

    vercel fix

---
+++ dist/test-out/d713d648-7a2d-418d-aa3e-155e242e9bf4/jasmine-cleanup-0.js
@@ -0,0 +1,17 @@
+// node_modules/@angular/build/src/builders/karma/polyfills/jasmine_global_cleanup.js
+(function() {
+  "use strict";
+  delete window.toString;
+})();
+/*! Bundled license information:
+
+@angular/build/src/builders/karma/polyfills/jasmine_global_cleanup.js:
+  (**
+   * @license
+   * Copyright Google LLC All Rights Reserved.
+   *
+   * Use of this source code is governed by an MIT-style license that can be
+   * found in the LICENSE file at https://angular.dev/license
+   *)
+*/
+//# sourceMappingURL=jasmine-cleanup-0.js.map
